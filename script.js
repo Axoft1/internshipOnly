a414a4baad6bed5c1c1b16ad361f6e503248d0e8
Aleksei Anufriev <axoft10@gmail.com>
2023-09-21 13:05:14 +0700

    left 1 event handler

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const wrapper = document.querySelector(".wrapper");
 const map = document.querySelector(".map");
 const root = document.querySelector(":root");
-const width = 41;
-root.style.setProperty("--point-width", `${width}px`);
+const WIDTH = 41;
+const HEIGHT = 41;
+root.style.setProperty("--point-width", `${WIDTH}px`);
+root.style.setProperty("--point-height", `${HEIGHT}px`);
 
 const points = [
   {
@@ -57,16 +59,29 @@ const points = [
   },
 ];
 
-map.onclick = function () {
+map.onclick = function (e) {
   const pointsEl = document.querySelectorAll(".point");
-  pointsEl.forEach(e => {
-    if (e.classList.contains("active")) {
-      e.classList.add("notActive");
-      e.classList.remove("active");
-      e.style.width = `${width}px`;
-    }
-    return;
-  });
+
+  if (e.target.classList.contains("notActive")) {
+    e.target.classList.remove("notActive");
+    e.target.classList.add("active");
+    e.target.style.width = `calc(${e.target.innerText.length}rem / 2 + ${
+      WIDTH + 16
+    }px)`;
+  } else if (e.target.classList.contains("active")) {
+    e.target.classList.remove("active");
+    e.target.classList.add("notActive");
+    e.target.style.width = `${WIDTH}px`;
+  } else {
+    pointsEl.forEach(e => {
+      if (e.classList.contains("active")) {
+        e.classList.add("notActive");
+        e.classList.remove("active");
+        e.style.width = `${WIDTH}px`;
+      }
+      return;
+    });
+  }
 };
 
 for (let i = 0; i < points.length; i++) {
@@ -76,25 +91,8 @@ for (let i = 0; i < points.length; i++) {
   map.innerHTML += `
   <div 
     class="point notActive" 
-    onclick="pointBtn(${i})" 
     style="top:${top}%; left:${left}%; background-color:${color}"
   >
     <p>${points[i].name}</p>
   </div>`;
-}
-
-function pointBtn(i) {
-  event.stopPropagation();
-  const pointsEl = document.querySelectorAll(".point");
-  if (pointsEl[i].classList.contains("notActive")) {
-    pointsEl[i].classList.remove("notActive");
-    pointsEl[i].classList.add("active");
-    pointsEl[i].style.width = `calc(${points[i].name.length}rem / 2 + ${
-      width + 16
-    }px)`;
-  } else {
-    pointsEl[i].classList.add("notActive");
-    pointsEl[i].classList.remove("active");
-    pointsEl[i].style.width = `${width}px`;
-  }
 }
